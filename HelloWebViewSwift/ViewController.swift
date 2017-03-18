3554b9cce741e1633957d0d009c586f5a2856aaa
user <user@Mac.local>
2017-03-18 16:06:06 +0800

    first commit

--- HelloWebViewSwift/ViewController.swift
+++ HelloWebViewSwift/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIWebViewDelegate {
 
+//我們假設一個url
+let iOS_URL = "https://www.google.com.tw/"
+    
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //webview 是否可滑超出邊界
+        webView.scrollView.bounces = false
+        
+        //是否讓網頁縮放至適合的大小
+        webView.scalesPageToFit = true
+        
+        //設置webView代理
+        webView.delegate = self
+        
+        //請求連結到指定的url
+        webView.loadRequest(URLRequest(url:URL(string:iOS_URL)!))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+// MARK: UIWebViewDelegate Support
+    
+    //網頁開始加載時
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        print("start")
+    }
+    //網頁加載結束時
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        print("finish")
+    }
+    //網頁加載失敗時
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print("error:"+error.localizedDescription)
+    }
+    //在網頁加載之前時
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        print("request:"+(request.url?.absoluteString)!)
+        return true
+    }
 
 }
 
